9d22afa7b56f078825a34621c096cf76d285a5cf
christophe chabin <chchabinsio@gmail.com>
2024-07-04 07:40:16 +0200

    Ajoutez des fichiers projet.

---
+++ libCptBqTU/Compte.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection.Metadata;
+
+namespace libCptBqTU
+{
+    public class Compte
+    {
+        /// <summary>
+        /// Propriétés implémentées automatiquement
+        /// </summary>
+        public int Numero { get; set; }
+        public string Nom { get; set; }
+        public decimal Solde { get; set; }
+        public decimal DecouvertAutorise { get; set; }
+
+
+        /// <summary>
+        /// Constructeur à 4 arguments
+        /// </summary>
+        /// <param name="numero">le numéro</param>
+        /// <param name="nom">le nom</param>
+        /// <param name="solde">le solde</param>
+        /// <param name="decouvertAutorise">le découvert autorisé</param>
+        public Compte(int numero, string nom, decimal solde, decimal decouvertAutorise)
+        {
+  
+        }
+        /// <summary>
+        /// Constructeur de compte par défaut
+        /// </summary>
+        public Compte()
+        {
+
+        }
+        /// <summary>
+        /// Réecriture de la méthode ToString
+        /// </summary>
+        /// <returns></returns>
+
+
+        /// <summary>
+        /// Crédite le compte du montant spécifié
+        /// </summary>
+        /// <param name="montant">Le montant à créditer</param>
+
+
+
+        /// <summary>
+        /// Débite le compte du montant spécifié si le solde le permet
+        /// </summary>
+        /// <param name="montant">Le montant à débiter</param>
+        /// <returns>True si le débit a été effectué, False sinon</returns>
+ 
+
+
+        /// <summary>
+        /// Transférer un montant vers un autre compte
+        /// </summary>
+        /// <param name="montant"></param>
+        /// <param name="compteDestination"></param>
+        /// <returns></returns>
+
+
+
+        /// <summary>
+        /// Savoir si le solde est supérieur à celui d'un autre compte
+        /// </summary>
+        /// <param name="compteDestination"></param>
+        /// <returns></returns>
+
+    }
+}
